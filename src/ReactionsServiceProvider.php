c20d096b657027baa63f92d41d5c07a45adfd0f4
Harish Kumar <harish.pra22@gmail.com>
2018-07-11 00:54:53 +0530

    package service provider

---
+++ src/ReactionsServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Hkp22\Laravel\Reactions;
+
+use Illuminate\Support\ServiceProvider;
+
+class ReactionsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrations();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+    * Load migrations.
+    *
+    * @return void
+    */
+    protected function loadMigrations()
+    {
+        if ($this->app->runningInConsole()) {
+            $migrationsPath = __DIR__ . '/../migrations';
+
+            $this->publishes([
+                $migrationsPath => database_path('migrations'),
+            ], 'migrations');
+
+            $this->loadMigrationsFrom($migrationsPath);
+        }
+    }
+}
