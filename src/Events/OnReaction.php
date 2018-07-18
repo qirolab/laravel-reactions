216a80c3023cf2cd03349779073259cc17b21fcd
Harish Kumar <harish.pra22@gmail.com>
2018-07-18 21:10:36 +0530

    OnReaction Event and OnDeleteReaction Event

---
+++ src/Events/OnReaction.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Hkp22\Laravel\Reactions\Events;
+
+use Hkp22\Laravel\Reactions\Models\Reaction;
+use Hkp22\Laravel\Reactions\Contracts\ReactsInterface;
+use Hkp22\Laravel\Reactions\Contracts\ReactableInterface;
+
+class OnReaction
+{
+    /**
+     * The reactable model.
+     *
+     * @var \Hkp22\Laravel\Reactions\Traits\Reactable
+     */
+    public $reactable;
+
+    /**
+     * User who reacted on model.
+     *
+     * @var \Hkp22\Laravel\Reactions\Traits\Reacts
+     */
+    public $reactBy;
+
+    /**
+     * Reaction model.
+     *
+     * @var \Hkp22\Laravel\Reactions\Models\Reaction
+     */
+    public $reaction;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  \Hkp22\Laravel\Reactions\Traits\Reactable $reactable
+     * @param  \Hkp22\Laravel\Reactions\Traits\Reacts    $reactBy
+     * @return void
+     */
+    public function __construct(ReactableInterface $reactable, Reaction $reaction, ReactsInterface $reactBy)
+    {
+        $this->reactable = $reactable;
+        $this->reaction = $reaction;
+        $this->reactBy = $reactBy;
+    }
+}
