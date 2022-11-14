aa2285dd898b93f0731bb72203e447f3ab046273
Harish Kumar <harish.pra22@gmail.com>
2022-11-14 23:13:54 +0530

    reactions table config test

---
+++ tests/Unit/ReactionsTableConfigTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Qirolab\Tests\Laravel\Reactions\Unit;
+
+use Qirolab\Laravel\Reactions\Models\Reaction;
+use Qirolab\Tests\Laravel\Reactions\TestCase;
+
+class ReactionsTableConfigTest extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        $app['config']->set('reactions.table_name', 'test-reactions');
+    }
+
+    /** @test **/
+    public function it_may_change_reactions_table_from_configuration()
+    {
+        $this->assertEquals(
+            config()->get('reactions.table_name'), 'test-reactions'
+        );
+
+        $this->assertEquals(
+            (new Reaction())->getTable(), 'test-reactions'
+        );
+
+        $article = $this->createArticle();
+        $user = $this->createUser();
+        $this->actingAs($user);
+        $article->react('like');
+
+        $this->assertEquals(1, $article->reactions->count());
+        $this->assertEquals($user->id, $article->reactions->first()->user_id);
+    }
+}
