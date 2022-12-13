72835bbba0c7e6168dc55a00732329fe6f232301
Süleyman Özgür Özarpacı <oozarpac@gmail.com>
2022-12-13 18:54:05 +0300

    optimize reaction summary (#35)

--- src/Traits/Reactable.php
+++ src/Traits/Reactable.php
@@ -50,8 +50,10 @@ trait Reactable
      */
     public function reactionSummary()
     {
-        return $this->reactions->groupBy('type')->map(function ($val) {
-            return $val->count();
+        return $this->reactions()->groupBy('type')->selectRaw('type, count(id) as total_count')->get()->mapWithKeys(function ($val) {
+            return [
+                $val->type => $val->total_count,
+            ];
         });
     }
 
